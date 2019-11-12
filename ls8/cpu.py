79df163d1e5ef7a18252e05f118691f7d1596f9e
micahjones13 <micahjones13@gmail.com>
2019-11-12 14:38:40 -0700

    day 2 MVP met, along with refactor of run with branchtable

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,6 +13,47 @@ class CPU:
         self.reg = [0] * 8
         #program counter
         self.pc = 0
+        #halt flag
+        self.halted = False
+        #binary values of commands
+        self.HLT = 0b00000001
+        #* Sets a specified register to a specified value, 3 byte instruction
+        self.LDI = 0b10000010
+        #* Prints the value of a register - 2 byte instruction
+        self.PRN = 0b01000111
+        #* Multiply the values in two registers together and store the reult in registerA - 3 byte
+        self.MULT = 0b10100010
+        #OPS
+        self.op1 = None
+        self.op2 = None
+        #branch table
+        self.branchtable = {}
+        self.branchtable[self.HLT] = self.handle_hlt
+        self.branchtable[self.PRN] = self.handle_prn
+        self.branchtable[self.LDI] = self.handle_ldi
+        self.branchtable[self.MULT] = self.handle_mult
+
+    def handle_hlt(self):
+        self.halted = True
+
+    def handle_prn(self):
+        print(self.reg[self.op1])
+
+        self.pc += 2
+
+    def handle_ldi(self):
+        print(f"Reg {self.op1} set: {self.op2}")
+        self.reg[self.op1] = self.op2
+        self.pc += 3
+    
+    def handle_mult(self):
+        self.alu("MULT", self.op1, self.op2)
+
+        self.pc += 3
+
+    def update_ops(self):
+        self.op1 = self.memory[self.pc + 1]
+        self.op2 = self.memory[self.pc + 2]
 
     def load(self):
         """Load a program into memory."""
@@ -21,21 +62,33 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
+        program_name = sys.argv[1]
 
-        for instruction in program:
-            #self.ram[address] = instruction
-            
-            self.memory[address] = instruction
-            address += 1
+        with open(program_name) as f:
+
+
+            for instruction in f:
+                #self.ram[address] = instruction
+                line = instruction.split("#")[0]
+                line = line.strip()
+                if line == "":
+                    continue
+                val = int(line, 2)
+
+                self.memory[address] = val #instruction
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -43,7 +96,9 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MULT":
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+            # print(self.reg[reg_a] * self.reg[reg_b])
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -89,41 +144,92 @@ class CPU:
         """
         Run the CPU
         """
-        #* Ends CPU operations
-        HLT = 0b00000001
-        #* Sets a specified register to a specified value, 3 byte instruction
-        LDI = 0b10000010
-        #* Prints the value of a register - 2 byte instruction
-        PRN = 0b01000111
+        # #* Ends CPU operations
+        # HLT = 0b00000001
+        # #* Sets a specified register to a specified value, 3 byte instruction
+        # LDI = 0b10000010
+        # #* Prints the value of a register - 2 byte instruction
+        # PRN = 0b01000111
+        # #* Multiply the values in two registers together and store the reult in registerA - 3 byte
+        # MULT = 0b10100010
 
-        #Loop flag
-        halted = False
-
-        while not halted:
-            #store the insturction --- at current(pc) of list in memory
+        
+        while not self.halted:
+            # print("PC", self.pc)
+            #Need this to update our operands to current based on the PC
+            self.update_ops()
+            # print(self.op1, self.op2)
+            #store insturction
             instruction = self.memory[self.pc]
-            #if instruction is halt, then exit
-            if instruction == HLT:
-                halted = True
+            #find that in the branchtable
+            to_run = self.branchtable[instruction]
+            #run it
+            to_run()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # print(bin(insturction), 'inst')
+        # while not self.halted:
+            #need to set an instruction
+            # self.branchtable[insturction]
+
+        # #Loop flag
+        # halted = False
+
+        # while not halted:
+        #     # print(self.pc)
+        #     # self.trace()
+        #     #store the insturction --- at current(pc) of list in memory
+        #     instruction = self.memory[self.pc]
+        #     #if instruction is halt, then exit
+        #     if instruction == HLT:
+        #         halted = True
+
+        #         self.pc += 1
+        #     #LDI takes LDI command, register, value
+        #     elif instruction == LDI:
+        #         reg_num = self.memory[self.pc + 1]
+        #         value = self.memory[self.pc + 2]
+        #         self.reg[reg_num] = value
+        #         print(f"Reg {reg_num} set: {self.reg[reg_num]}")
+
+        #         self.pc += 3
+        #     elif instruction == PRN:
+        #         #PC at current is the command(PRN, or 3 in this case) pc + 1 is the operands for command
+        #         reg_num = self.memory[self.pc + 1]
+        #         print(self.reg[reg_num])
+
+        #         self.pc += 2
+        #     elif instruction == MULT:
+        #         # reg_num1 = self.memory[self.pc + 1]
+        #         # # print(reg_num1, 'regnum1')
+        #         # reg_num2 = self.memory[self.pc + 2]
                 
-                self.pc += 1
-            #LDI takes LDI command, register, value
-            elif instruction == LDI:
-                reg_num = self.memory[self.pc + 1]
-                value = self.memory[self.pc + 2]
-                self.reg[reg_num] = value
+        #         self.alu("MULT", self.memory[self.pc + 1], self.memory[self.pc + 2])
+                
 
-                self.pc += 3
-            elif instruction == PRN:
-                #PC at current is the command(PRN, or 3 in this case) pc + 1 is the operands for command
-                reg_num = self.memory[self.pc + 1]
-                print(self.reg[reg_num])
-
-                self.pc += 2
+        #         self.pc += 3
             
 
-            #catch all
-            else:
-                print(f"Uknown instruction at indexd {self.pc}")
-                halted = True
+        #     #catch all
+        #     else:
+        #         print(f"Uknown instruction at indexd {self.pc}")
+        #         halted = True
 
